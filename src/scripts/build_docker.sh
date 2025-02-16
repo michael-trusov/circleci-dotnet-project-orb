315a840eec2f64adb31c61277f152347eda744f9
Michael Trusov <trusov.michael@gmail.com>
2025-02-17 00:13:22 +0200

    Add dv and sv for docker functionality

--- src/scripts/build_docker.sh
+++ src/scripts/build_docker.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 #shellcheck disable=all
 
+### Description
+###   This function print message if INPUT_PARAM_DEBUG is set to 'true'
+###
+### Parameters:
+###   $1 - message
+debug_message() {
+    local message=$1
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message"
+    fi
+}
+
+if [ -z "$IMAGE_TAG" ]; then
+    if [ -z "${INPUT_TAG_FILE}" ]; then
+        debug_message "IMAGE_TAG is not specified and file with tag information is not provided."
+        exit 1
+    elif [ ! -f "${INPUT_TAG_FILE}" ]; then
+        debug_message "INPUT_TAG_FILE '${INPUT_TAG_FILE}' does not exist."
+        exit 1
+    else
+        IMAGE_TAG=$(tr -d ' ' < "${INPUT_TAG_FILE}")
+        debug_message "IMAGE_TAG is set to '${IMAGE_TAG}' from file '${INPUT_TAG_FILE}'."
+    fi
+fi
+
 docker build -f ${DOCKERFILE} -t ${ACR_URL}/${IMAGE_NAME}:${IMAGE_TAG} ${DOCKER_CONTEXT}
--- src/scripts/push_docker.sh
+++ src/scripts/push_docker.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 #shellcheck disable=all
 
+debug_message() {
+    local message=$1
+
+    if [[ "${INPUT_PARAM_DEBUG}" == "true" ]]; then
+        echo "[DEBUG] $message"
+    fi
+}
+
+if [ -z "$IMAGE_TAG" ]; then
+    if [ -z "${INPUT_TAG_FILE}" ]; then
+        debug_message "IMAGE_TAG is not specified and file with tag information is not provided."
+        exit 1
+    elif [ ! -f "${INPUT_TAG_FILE}" ]; then
+        debug_message "INPUT_TAG_FILE '${INPUT_TAG_FILE}' does not exist."
+        exit 1
+    else
+        IMAGE_TAG=$(tr -d ' ' < "${INPUT_TAG_FILE}")
+        debug_message "IMAGE_TAG is set to '${IMAGE_TAG}' from file '${INPUT_TAG_FILE}'."
+    fi
+fi
+
+
 docker login -u $ACR_USERNAME -p $ACR_PASSWORD $ACR_URL
 docker push ${ACR_URL}/${IMAGE_NAME}:${IMAGE_TAG}
