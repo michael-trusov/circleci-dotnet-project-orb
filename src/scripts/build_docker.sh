67069f343ac71b5619d5c2b7c09b31eed9ae9f73
Michael Trusov <trusov.michael@gmail.com>
2025-02-16 19:21:25 +0200

    Test

--- src/scripts/build_docker.sh
+++ src/scripts/build_docker.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 #shellcheck disable=all
 
-echo "CONATINER_REGISTRY_URL: ${CONTAINER_REGISTRY_URL}"
-echo "ACR_URL: ${ACR_URL}"
-echo "IMAGE_NAME: ${IMAGE_NAME}"
-echo "IMAGE_TAG: ${IMAGE_TAG}"
-echo "DOCKERFILE: ${DOCKERFILE}"
-
 docker build -f ${DOCKERFILE} -t ${ACR_URL}/${IMAGE_NAME}:${IMAGE_TAG} ${DOCKER_CONTEXT}
--- src/scripts/pack_and_push_helm.sh
+++ src/scripts/pack_and_push_helm.sh
@@ -5,7 +5,7 @@ helmRepoArtifact="${HELM_REPO}/${HELM_CHART_NAME}:${HELM_CHART_VERSION}"
 
 # package and publish helm chart
 helm version
-helm registry login $CONTAINER_REGISTRY_URL --username $CONTAINER_REGISTRY_USERNAME --password $CONTAINER_REGISTRY_PASSWORD
+helm registry login $ACR_URL --username $ACR_USERNAME --password $ACR_PASSWORD
 helm package --app-version="${HELM_CHART_NAME}:${HELM_CHART_VERSION}" --version=1.0.0 k8s/helm/$HELM_CHART_NAME
 helm chart save "${HELM_CHART_NAME}-1.0.0.tgz" $helmRepoArtifact
 helm chart push $helmRepoArtifact
